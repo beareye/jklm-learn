68e543edbce0493546860b2e21c06f126926f72e
jeff <jjeffreywang@gmail.com>
2021-05-18 00:03:19 -0700

    added extra letter search function

--- script.bash
+++ script.bash
@@ -2,9 +2,16 @@
 numberOfReturnValues=${1-30}
 while True
 do
-    read syllable
-    echo -e "$syllable \n"
-    grep $syllable words.txt > temp.txt
+    read args
+    args_temp=( $args )
+    syllable=${args_temp[0]}
+    letter=${args_temp[1]}
+    if [ -z "$letter" ]     
+    then
+        grep -i $syllable words.txt > temp.txt
+    else
+        grep -i $syllable words.txt | grep -i $letter > temp.txt
+    fi
     shuf -n $numberOfReturnValues temp.txt
     echo -e "======================================END======================================"
     echo -e "\n\n\n\n\n"
